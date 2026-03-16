5065595907cb7bc36722dfdb2d9e318fd4e606cd
DanielCortesGH <danielcortesvillagh@gmail.com>
2026-03-16 06:05:10 +0000

    Variables

---
+++ S2_04_Variables.py
@@ -0,0 +1,37 @@
+
+#LAB
+jhon=7
+mary=8
+adam=14
+total_apples = jhon + mary + adam
+print(total_apples)
+
+#Operadores Abreviados
+
+# x+=1   ->  Se suma n(cualquier numero) a la variable
+# x/=2   ->  Se divide n veces la variable
+# x %=10 ->  Se aplica modulo n a la variable
+# x **=2 ->  Se eleva a la potencia n la variable
+
+#LAB 2
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = miles * 1.61
+kilometers_to_miles = kilometers / 1.61
+
+print(miles, "millas son", round(miles_to_kilometers, 2), "kilómetros")
+print(kilometers, "kilómetros son", round(kilometers_to_miles, 2), "millas")
+
+#LAB 3
+x1 = 0
+x2 = 1
+x3 = -1
+
+Y1 = 3*x1**3-2*x1**2+3*x1-1
+Y2 = 3*x2**3-2*x2**2+3*x2-1
+Y3 = 3*x3**3-2*x3**2+3*x3-1
+
+print("1.Y = ",Y1)
+print("2.Y = ",Y2)
+print("3.Y = ",Y3)
