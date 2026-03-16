5d0f7ca92b2974e92251d715491d234b56f64f23
DanielCortesGH <danielcortesvillagh@gmail.com>
2026-03-16 02:34:02 +0000

    Print

---
+++ 01_print.py
@@ -0,0 +1,11 @@
+#Función basica 
+print("HOLA MUNDO")
+
+#Palabras reservadas SEP Y END "LAB"
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python")
+#LAB Dando formato a la salida "Flecha"
+print("     *     \n    * *   \n   *   *   \n  *     *\n    * * \n    * * \n    * *  ")
+#LAB Ahora flecha doble 
+print("     *     \n    * *   \n   *   *   \n  *     *\n    * * \n    * * \n    * *  "*2)
+
