832d24cde2ee080928b2ab46b5fcdab5a3e90058
DanielCortesGH <danielcortesvillagh@gmail.com>
2026-03-16 03:47:37 +0000

    OpNum

---
+++ S2_03_OperadoresNum.py
@@ -0,0 +1,23 @@
+#Suma
+print(1+1) 
+
+#Resta
+print(9-3) 
+
+#Multiplicacion
+print(12*8) 
+
+#Exponente (3 elevado a la 3)
+print(2**3) 
+
+#Division
+print(10/2) 
+
+#Modulo
+print(69%13) 
+
+#Division sin decimales
+print(123//4) 
+
+#Raiz (Exponente con numeros menores a 1 y mayores a 0)
+print(16**.5) 
